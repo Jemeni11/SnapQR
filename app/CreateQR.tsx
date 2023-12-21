5cd3d745ae3e823647ebe91ac5f157ede7bf126b
Emmanuel Jemeni <jemenichinonso11@gmail.com>
2023-12-21 16:18:08 +0100

    feat(CreateQR.tsx): Add new screen

---
+++ app/CreateQR.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const CreateQR = () => {
+  return (
+    <View>
+      <Text>CreateQR</Text>
+    </View>
+  );
+};
+
+export default CreateQR;
+
+const styles = StyleSheet.create({});
