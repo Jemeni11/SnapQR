9ae7f0fb1b9645404e4d5164085acf76d5eb4b08
Emmanuel Jemeni <jemenichinonso11@gmail.com>
2023-12-27 13:33:59 +0100

    Add HomeScreen component

---
+++ app/index.tsx
@@ -0,0 +1,51 @@
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import React from "react";
+import { GlobalStyles } from "../styles";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import TabScreen from "../components/TabScreen";
+import PageTitle from "../components/PageTitle";
+import { Ionicons } from "@expo/vector-icons";
+
+export default function HomeScreen({ navigation }) {
+  const insets = useSafeAreaInsets();
+
+  function HomeButton({ onClick, text }) {
+    return (
+      <TouchableOpacity style={styles.homeButton} onPress={onClick}>
+        <Text style={styles.homeButtonText}>{text}</Text>
+        <Ionicons
+          name="arrow-forward"
+          size={30}
+          color={GlobalStyles.color.secondaryColor}
+        />
+      </TouchableOpacity>
+    );
+  }
+
+  return (
+    <TabScreen extraStyles={{ paddingTop: insets.top }}>
+      <PageTitle title="Home" />
+      <View>
+        <HomeButton
+          text="Scan with the Camera"
+          onClick={() => navigation.navigate("ScanFromCameraScreen")}
+        />
+      </View>
+    </TabScreen>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {},
+  homeButton: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginVertical: 8,
+  },
+  homeButtonText: {
+    fontFamily: GlobalStyles.fonts.regular,
+    fontSize: 25,
+    color: GlobalStyles.color.secondaryColor,
+  },
+});
