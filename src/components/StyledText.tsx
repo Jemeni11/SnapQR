98bbfb56fd7dbcc12a5993fdc4150a9939b12d87
Emmanuel C. Jemeni <jemenichinonso11@gmail.com>
2026-05-04 15:30:07 +0100

    ran oxlint, changed linter's config, deleted unneeded files

--- src/components/StyledText.tsx
+++ src/components/StyledText.tsx
@@ -1,5 +1,13 @@
+import { StyleSheet } from "react-native";
+
 import { Text, TextProps } from "./Themed";
 
+const StyledTextStyle = StyleSheet.create({
+  monoText: {
+    fontFamily: "SpaceMono",
+  },
+});
+
 export function MonoText(props: TextProps) {
-  return <Text {...props} style={[props.style, { fontFamily: "SpaceMono" }]} />;
+  return <Text {...props} style={[props.style, StyledTextStyle.monoText]} />;
 }
