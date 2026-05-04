9faca4213922227cabc99f977c75b647d315a07b
Emmanuel C. Jemeni <jemenichinonso11@gmail.com>
2026-05-04 14:05:21 +0100

    minor change

--- src/app/+not-found.tsx
+++ src/app/+not-found.tsx
@@ -1,14 +1,14 @@
-import { Link, Stack } from 'expo-router';
-import { StyleSheet } from 'react-native';
+import { Link, Stack } from "expo-router";
+import { StyleSheet } from "react-native";
 
-import { Text, View } from '@/components/Themed';
+import { Text, View } from "@/components/Themed";
 
 export default function NotFoundScreen() {
   return (
     <>
-      <Stack.Screen options={{ title: 'Oops!' }} />
+      <Stack.Screen options={{ title: "Oops!" }} />
       <View style={styles.container}>
-        <Text style={styles.title}>This screen doesn't exist.</Text>
+        <Text style={styles.title}>This screen doesn't exist. How did you get here?</Text>
 
         <Link href="/" style={styles.link}>
           <Text style={styles.linkText}>Go to home screen!</Text>
@@ -20,14 +20,10 @@ export default function NotFoundScreen() {
 
 const styles = StyleSheet.create({
   container: {
+    alignItems: "center",
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
     padding: 20,
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
   },
   link: {
     marginTop: 15,
@@ -35,6 +31,10 @@ const styles = StyleSheet.create({
   },
   linkText: {
     fontSize: 14,
-    color: '#2e78b7',
+    color: "#2e78b7",
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
   },
 });
